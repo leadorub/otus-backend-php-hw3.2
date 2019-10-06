14d27afd7a4eed74afda126dbd3ce4468a40e567
xpeh <xpeh@harddays.net>
2019-10-06 20:54:11 +0300

    initial version of homework 3.2

---
+++ README.md
@@ -0,0 +1,5 @@
+# Homework 3.2
+## Course "Otus. PHP backend developer"
+
+Setup:
+composer install
---
+++ src/AI/Lesson4/HelloWorld.php
@@ -0,0 +1,10 @@
+<?php
+namespace AI\backend_php_hw3_2\Lesson4;
+
+class HelloWorld
+{
+    public function sayHello()
+    {
+        echo "Hello, World from <tt>".__METHOD__."</tt>";
+    }
+}
